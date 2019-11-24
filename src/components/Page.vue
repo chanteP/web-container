e7d654e301dd3307d831a021606b37367824360d
chantep <541149506@qq.com>
2019-11-25 01:25:14 +0800

    page animate

--- src/components/Page.vue
+++ src/components/Page.vue
@@ -1,29 +1,36 @@
 <template>
   <div class="page">
     <div class="navigation-bar">
-      <span class="button-ll"></span>
       {{title}}
+      <span v-if="index > 0" class="button button-ll" data-act="back" @click="goback"></span>
     </div>
-    <iframe :src="src" class="webview"></iframe>
+    <iframe :src="src" ref="iframe" @load="iframeOnLoad" class="webview"></iframe>
   </div>
 </template>
 <script lang="ts">
-import { createComponent, PropType, reactive, computed } from '@vue/composition-api'
+import { createComponent, PropType, reactive, computed, ref, onMounted } from '@vue/composition-api'
 
 export default createComponent({
   props: {
     url: String,
+    index: Number
   },
-  setup(props) {
+  setup(props, context) {
     const state = reactive({
       title: 'title'
-    });
+    })
     const src = computed(() => {
-      return props.url;
-    });
+      return props.url
+    })
+    function iframeOnLoad(e: Event) {
+      const iframe = e.target as HTMLIFrameElement
+      state.title = iframe.contentWindow!.document.title || ''
+    }
     return {
       ...state,
       src,
+      goback: () => history.go(-1),
+      iframeOnLoad
     }
   }
 })
@@ -38,25 +45,32 @@ export default createComponent({
   outline: 1px solid #999;
   background: #fff;
 }
-.navigation-bar{
+.navigation-bar {
+  position: relative;
   height: 64px;
   line-height: 64px;
   border-bottom: 1px solid #aaa;
   text-align: center;
   font-size: 20px;
 }
-.button-ll{
+.button-ll {
   position: absolute;
   left: 20px;
-  top: 23px;
+  top: 50%;
   display: inline-block;
+  transform: translate(0, -50%);
+  cursor: pointer;
+}
+[data-act='back']:before {
+  content: '';
+  display: block;
   width: 14px;
   height: 14px;
   border-top: 1px solid #666;
   border-left: 1px solid #666;
   transform: rotate(-45deg);
 }
-.webview{
+.webview {
   border: none;
   width: 100%;
   height: calc(100% - 64px);
--- src/components/ViewPort.vue
+++ src/components/ViewPort.vue
@@ -1,17 +1,49 @@
 <template>
-  <div class="viewport">
-    <page></page>
+  <div class="viewport" @click="push">
+    <div class="page-list-container">
+      <transition-group name="page-list" tag="div">
+        <page v-for="(url, index) in stack" :url="url" :index="index" :key="url"></page>
+      </transition-group>
+    </div>
+
+    <div>
+      <ul class="stack-info">
+        <li v-for="(url, index) in stack" :key="url">
+          {{`[${index}] ${url}`}}
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 <script lang="ts">
-import { createComponent, PropType } from '@vue/composition-api'
-import Page from './Page.vue';
+import { createComponent, PropType, reactive } from '@vue/composition-api'
+import page from './Page.vue'
 
 export default createComponent({
-  components: {Page},
+  components: { page },
   props: {},
   setup(props) {
-    return {}
+    const state = reactive({
+      stack: [] as string[]
+    })
+
+    function pushPage(url: string, title = '') {
+      history.pushState({}, title, url)
+      state.stack.push(url)
+    }
+    function popPage() {
+      state.stack.pop()
+    }
+    function clearTo() {}
+
+    window.addEventListener('popstate', popPage)
+
+    return {
+      ...state,
+      push: () => {
+        pushPage(`/pages/${Math.random()}`)
+      }
+    }
   }
 })
 </script>
@@ -20,13 +52,38 @@ export default createComponent({
 .viewport {
   position: relative;
   top: 50%;
-  left: 10%;
+  margin-left: 10%;
+  padding-left: 375px;
+  height: 667px;
+  transform: translate(0, -50%);
+  overflow: visible;
+}
+.page-list-container {
+  position: absolute;
+  left: 0;
   width: 375px;
-  height: 812px;
+  height: 100%;
   border: 1px solid #999;
   box-shadow: rgba(0, 0, 0, 0.3) 0 0 40px;
-  background: #fff;
-  transform: translate(0, -50%);
+  background: #fcfcfc;
   overflow: hidden;
+}
+
+.page-list-enter-active,
+.page-list-leave-active {
+  transition: all 0.5s ease;
+}
+.page-list-enter, .page-list-leave-to
+/* .list-leave-active for below version 2.1.8 */ {
+  transform: translateX(100%);
+}
+
+.stack-info{
+  margin: 0;
+  font-size: 12px;
+  color: #999;
+}
+.stack-info li:hover{
+  background: rgba(144,144,144,.2);
 }
 </style>
